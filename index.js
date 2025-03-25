6a4fc6eb98980aadde938c728d08cb74e861d2c2
Sarmad Faizan <sarmadf49@gmail.com>
2025-03-26 00:29:29 +0500

    server connection and schema file

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const express = require('express');
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+ 
+
+//server setup
+const server = new ApolloServer({
+    // typeDefs -> type definitions
+    // resolvers -> resolver functions
+})
+
+const {url} = await startStandaloneServer(server, {
+    listen:{port: 4000}
+})
+
+console.log(`🚀 Server ready at: `, 4000)
