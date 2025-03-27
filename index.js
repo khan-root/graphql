c6518dd6d7a9015e96890c85c27ca8d45c1cad4e
Sarmad Faizan <sarmadf49@gmail.com>
2025-03-28 02:46:45 +0500

    fetch data from placeholder

--- index.js
+++ index.js
@@ -1,15 +1,72 @@
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { expressMiddleware } from "@apollo/server/express4";
+import express from 'express'
+import cors from 'cors'
+import axios from "axios";
  
 
-//server setup
-const server = new ApolloServer({
-    // typeDefs -> type definitions
-    // resolvers -> resolver functions
-})
+async function startServer(){
+    const app = express()
+    app.use(express.json())
+    app.use(cors())
+    //server setup
+    const server = new ApolloServer({
+        typeDefs:`
+            type Todo {
+                id: ID!
+                userId: Int!
+                title: String!
+                completed: Boolean
+                user: User
+            }
 
-const {url} = await startStandaloneServer(server, {
-    listen:{port: 4000}
-})
+            type User {
+                id: ID!
+                name: String! 
+                username: String!
+                email: String!
+            }
 
-console.log(`🚀 Server ready at: `, 4000)
+
+            type Query{
+                getTodos: [Todo]
+                getUsers: [User]
+                getSingleUser(id: ID!): User
+            }
+        `,
+
+        resolvers:{
+
+            Todo:{
+
+                user: async(todo)=>{
+                    try{
+                        const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${todo.userId}`)
+                        const data = response.data 
+                        return data 
+                    }catch(err){
+                        console.log(err.message)
+                    }
+                }
+            
+            },
+            Query:{
+                getTodos:async()=> (await axios.get("https://jsonplaceholder.typicode.com/todos")).data,
+                getUsers: async()=>(await axios.get("https://jsonplaceholder.typicode.com/users")).data,
+                getSingleUser: async(parent, {id})=>{
+                    return (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)).data
+                }
+            }
+        }
+    })
+    await server.start()
+    app.use("/graphql", expressMiddleware(server))
+
+
+    app.listen(8000, ()=>{
+        console.log('Server Start on 8000 ')
+    })
+}
+
+
+startServer()
